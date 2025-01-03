590033610cbfaf890c8032acb2d8fbc61298bec1
Nausheen Nazim <nausheen.nazim@gmail.com>
2025-01-04 01:53:57 +0800

    test links

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "../styles/HeroSection.css";
 import homeBanner from "../assets/home-banner.png"
+import { Link } from "react-router-dom";
 
 const HeroSection = () => {
   return (
     <div className="hero-container" >
       <img src={homeBanner} />
-      <button type="button" onClick={() => { window.location.href = '/newArrivals' }}>
-        Shop Now
+      <button type="button">
+        <Link to="/newArrivals">Shop Now</Link>
       </button>
       <div className="hero-content">
         <h1>FIND CLOTHES</h1>
